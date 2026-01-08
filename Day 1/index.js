8f9ac9cdcf9256ea3a31f8a56b1fa91b802b6552
sranveer1021 <ranveersingh21july@gmail.com>
2026-01-08 22:18:16 +0530

    This is my JavaScript programs

---
+++ Day 1/index.js
@@ -0,0 +1,117 @@
+// // variable ko kaise banate hai 
+
+// // let name = "Ranveer";
+// // let age = 20;
+// // age = 30;
+// // console.log(age);
+
+// // const account = 1234;
+// // account = 23;
+
+// // console.log(account);
+
+
+// // var a = 10;
+// // var a = 20;
+// // console.log(a);
+
+// // data types
+
+// // primitive data types
+
+// // number,string,boolean,undefined,null,bigint,symbol
+
+// // number
+// let a = 10;
+// let b = 2.36;
+// console.log(a,b);
+
+// // string
+
+// let c = "strike i coming";
+// let d = "Anjali";
+// console.log(c,d);
+
+// // boolean
+
+// let login = true;
+// let f = false;
+// console.log(login,f);
+
+// // undefined
+
+// let user;
+// // const p;
+// console.log(user);
+
+// // bigint
+// let num = 23344343344432432324343242n;
+// console.log(num);
+
+// null 
+
+// let weather = null;
+
+//let weather = current_weather("Dwarka");
+// 25
+// null
+// undefined
+
+// symbol
+
+// const id1 = Symbol("1d");
+// const id2 = Symbol("id");
+// console.log(id2==id1);
+
+
+
+
+// Non primitive data types
+// array,object,function
+
+// let arr = [10,20,11,"ranveer",true];
+
+//console.log(arr);
+
+// Ranveer 12313 23 gen
+
+// let user = {
+//     name :"Ranveer",
+//     account : 12313,
+//     age :24,
+//     category:'gen'
+
+// }
+
+
+//  let s = function add(){
+//     console.log("Hello");
+// }
+// s();
+
+
+// primitive data types is immulabe
+
+let a = 10;
+a = 20;
+
+let str = "Ranveer";
+str = "Mohan";
+
+console.log(str);
+
+// Non primitive data type mutable
+
+let arr = [10,20,30,40];
+
+arr.push(90);
+arr[0]=(40);
+console.log(arr);
+
+let obj = {
+    name: "Ranveer",
+    age: 24
+}
+obj.name = "rohan",
+
+console.log(obj);
