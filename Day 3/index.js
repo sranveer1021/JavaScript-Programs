e750fbf439f1432172a770ed8d7695647f2772e7
sranveer1021 <ranveersingh21july@gmail.com>
2026-01-09 21:10:10 +0530

    this is my 3rd lecture

---
+++ Day 3/index.js
@@ -0,0 +1,77 @@
+// Number
+// let a = 10;
+// let b = 345.6821;
+// let c = b.toFixed(1);
+// console.log(typeof c);
+
+// console.log(b.toPrecision(4));
+// console.log(b.toString());
+
+
+// 
+
+// let a = new Number(20);
+// let b = new Number(20);
+
+// console.log(a==b);
+
+// console.log(Boolean(null));
+
+// let b = 20;
+
+// console.log(typeof b);
+
+// let obj1 = {
+//     name:"Rohit"
+// }
+
+// let obj2 = obj1;
+
+// let obj2 = {
+//     name:"Rohit"
+// }
+
+// console.log(obj1==obj2);
+
+// Non Primitive: Reference bases pe compare
+// Primitive: Copy by value
+// let a = 10;
+// let b = a;
+
+// console.log(a==b);
+
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.SQRT2);
+// console.log(Math.ceil(6.3));
+// console.log(Math.floor(6.3));
+// console.log(Math.log10(20));
+// console.log(Math.max(20,11,3421,12));
+// console.log(Math.random());
+// [0,1): It will generate value between 0 and 1, where 0 is included but 1 is not included
+
+
+// Satebaaji ek game banata:(0-9)
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+// console.log(Math.floor(Math.random()*6)+1);
+
+// Math.floor(Math.random()*totalNumberOfOutcome)+shift
+
+
+// 15-25
+
+
+// console.log(Math.floor(Math.random()*11)+15)
+
+// Math.floor(Math.random()*(max-min+1))+min
+
+// OTP Generate: 4 digit: 1000-9999
+
+
+// console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
+
