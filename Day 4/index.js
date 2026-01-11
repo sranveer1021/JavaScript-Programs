e164917fa28f09bd9b485c964c4617066695a822
sranveer1021 <ranveersingh21july@gmail.com>
2026-01-11 23:39:11 +0530

    this is my string lecture

---
+++ Day 4/index.js
@@ -0,0 +1,91 @@
+// String
+// const str1 = "Rohitboy";
+// const str2 = 'Rohit Negi';
+// const day = 18;
+// const str3 = `Strike is coming on ${day}`;
+
+// console.log(str3);
+
+// const str = `Hello Coder Army`;
+
+// console.log(str.length)
+// console.log(str[1]);
+
+// str[2] = "S";
+// console.log(str)
+
+// upper case
+// const a = str.toUpperCase();
+// const b = str.toLowerCase();
+// console.log(b);
+
+// const str = `Hello Coder Army Coder`;
+
+// console.log(str.indexOf('Cod'));
+// console.log(str.lastIndexOf('Cod'));
+// console.log(str.includes('cod'));
+
+// slice 
+// console.log(str.slice(2,7));
+// console.log(str.slice(3));
+// console.log(str.slice(-5,-2));
+
+// const str = `Hello Coder Army Coder`;
+
+// console.log(str.substring(2,5));
+
+// const a = "Rohit";
+// const b = "Negi";
+// const c = a+" "+b;
+// console.log(c);
+
+// console.log(24+"Rohit");
+// console.log(24+"Rohit"+10);
+// console.log(24+30+"Rohit");
+
+// const str = `Hello Coder Army Coder`;
+
+// console.log(str.replaceAll("ode",'iam'));
+
+// const user = "  Rohit  Negi ";
+// console.log(user.trim());
+
+// const names = "Rohit Mohit Suraj Rohan Anjali";
+
+// console.log(names.split(" "))
+// console.log(names.split(","))
+
+// const now = new Date();
+
+// console.log(now);
+// console.log(now.toString());
+// console.log(now.toISOString());
+// console.log(now.toLocaleString());
+
+
+// Local time chal rha
+// console.log(now.getDay());
+// console.log(now.getDate());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getHours());
+// console.log(now.getSeconds());
+
+
+// days: Mon-tue(1 based)
+// Month: 0 1
+
+// year month date hour minute second millisecond
+// const now = new Date(2025,8,20,8,25,16,125);
+
+// console.log(now.toString());
+
+
+// Timestamp
+const now = Date.now();
+const dates = new Date(1759275037293);
+// console.log(dates.toString());
+
+console.log(dates);
+console.log(now);
+// 1759272851288: TimeStamp
